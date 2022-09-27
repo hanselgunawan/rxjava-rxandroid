90b9962a90ce380f939d9c284ff8cee9c4d7a776
Hansel Tritama <hansel.tritama@nortonlifelock.com>
2022-09-26 23:38:57 -0700

    RxJava - Introduction

---
+++ app/src/main/java/com/example/rxjava_rxandroid/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.rxjava_rxandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "myApp"
+    private val greeting: String = "Hello from RxJava"
+    private lateinit var myObservable: Observable<String>
+
+    private lateinit var myObserver: Observer<String>
+
+    private lateinit var myText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myText = findViewById(R.id.tvGreeting)
+
+        myObservable = Observable.just(greeting)
+
+        myObserver = object : Observer<String> {
+            override fun onSubscribe(d: Disposable) {
+                Log.i(TAG, "onSubscribe invoked")
+            }
+
+            override fun onNext(t: String) {
+                Log.i(TAG, "onNext invoked")
+                myText.text = t
+            }
+
+            override fun onError(e: Throwable) {
+                Log.i(TAG, "onError invoked")
+            }
+
+            override fun onComplete() {
+                Log.i(TAG, "onComplete invoked")
+            }
+
+        }
+
+        // Observable won't emit data until someone subscribes to it
+        myObservable.subscribe(myObserver)
+    }
+}
